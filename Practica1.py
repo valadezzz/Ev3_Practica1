99adeab7b1bb602000ce42a5f9e5c7035d4f4835
valadezzz <eliud12isai@gmail.com>
2026-04-15 08:47:50 -0600

    se agregaron los atributos

--- Practica1.py
+++ Practica1.py
@@ -1,2 +1,8 @@
 class Salud:
-    
+    def __init__ (self,nombre,edad,peso,altura,sexo):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__peso = peso
+        self.__altura = altura
+        self.__sexo = sexo
+        
